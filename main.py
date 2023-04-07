ecf9254a9a2bfa33bdb7e3deb0969644c815a28b
fu-kunn <sky53908@gmail.com>
2023-04-07 15:54:06 +0900

    [Add]パスパラメータの優先順位

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = FastAPI()
 # ルートにアクセスがあったら実行する
 # async→非同期処理
 @app.get("/countries/{country_name}")
-async def country(country_name):
+async def country(country_name: str):
     return {"country_name": country_name}
+
+@app.get("/countries/japan")
+async def country():
+    return {"message": "This is Japan"}
