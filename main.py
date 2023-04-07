970b6d118ef5b6f2e9a507b07af5992a3c5fb045
fu-kunn <sky53908@gmail.com>
2023-04-07 16:08:15 +0900

    [Add]クエリパラメータの変更

--- main.py
+++ main.py
@@ -5,10 +5,17 @@ app = FastAPI()
 
 # ルートにアクセスがあったら実行する
 # async→非同期処理
-@app.get("/countries/{country_name}")
-async def country(country_name: str):
-    return {"country_name": country_name}
+"""
+クエリパラメータ
+?country_name=America&country_no=10
+"""
+@app.get("/countries/")
+async def country(country_name: str = "japan", country_no: int = 1):
+    return {
+        "country_name": country_name,
+        "country_no": country_no
+    }
 
-@app.get("/countries/japan")
-async def country():
-    return {"message": "This is Japan"}
+# @app.get("/countries/japan")
+# async def country():
+#     return {"message": "This is Japan"}
