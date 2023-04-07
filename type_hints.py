575010e3223d8ac1a97d58759da0ba60e3fd8f5f
fu-kunn <sky53908@gmail.com>
2023-04-07 14:52:18 +0900

    [Add]仮想環境の作成

--- type_hints.py
+++ type_hints.py
@@ -1,14 +1,22 @@
+from typing import List, Dict
+
+sample_list: List[int] = [1, 2, 3, 4]
+sample_dict: Dict[str, str] = {"username": "abcd"}
+
+print(type(sample_list[0]))
+print(sample_dict)
+
 """
 型ヒント
 : int
 : float
 """
 
-price: int = 100
-tax: float = 1.1
+# price: int = 100
+# tax: float = 1.1
 
-def calc_price_including_tax(price: int, tax: float) -> int:
-    return int(price*tax)
+# def calc_price_including_tax(price: int, tax: float) -> int:
+#     return int(price*tax)
 
-if __name__ == "__main__":
-    print(f"{calc_price_including_tax(price=price, tax=tax)}円")
+# if __name__ == "__main__":
+#     print(f"{calc_price_including_tax(price=price, tax=tax)}円")
